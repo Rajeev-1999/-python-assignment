37cff4941f86933bf4f23ed828cf4f8ca57dfc68
Rajeev <rajeevkumaar1999@gmail.com>
2025-05-01 21:34:04 +0530

    Initial commit

---
+++ index.py
@@ -0,0 +1,23 @@
+# Question One 
+number_one = int(input("Enter Number one : "))
+number_two = int(input("Enter Number two : "))
+
+Addition = number_one + number_two
+Subtraction = number_one - number_two
+Multiplication = number_one * number_two
+Division = number_one / number_two
+
+print("Addition : ",Addition)
+print("Subtraction : ",Subtraction)
+print("Multiplication : ",Multiplication)
+print("Division : ",Division)
+
+
+# Question Two
+
+first_name = input("Enter you first name : ")
+last_name = input("Enter last name here : ")
+
+full_name = first_name + " " + last_name
+
+print(full_name)
